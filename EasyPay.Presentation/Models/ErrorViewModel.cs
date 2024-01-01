f216de0908ad9dd5b558ac8d2accce0899ac1a29
NursultanSultan <tu201906251@code.edu.az>
2024-01-01 16:16:36 +0400

    'feat: build project structure'

--- EasyPay.Presentation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EasyPay.Presentation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
