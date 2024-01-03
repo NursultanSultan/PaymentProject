b652d4282c36bfdcd7f1d26556e7f369be999aca
NursultanSultan <tu201906251@code.edu.az>
2024-01-03 14:52:19 +0400

    'feat : added customer account'

---
+++ EasyPay.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.Entity.Concrete
+{
+    public class CustomerAccount
+    {
+        public int AccountID { get; set; }
+        public string AccountNumber { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+        public string BankBranch { get; set; }
+    }
+}
