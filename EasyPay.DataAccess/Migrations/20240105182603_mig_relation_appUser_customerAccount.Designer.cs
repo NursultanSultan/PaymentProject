6f8575b2e8564933ee3133f913f9ad96fff123cf
NursultanSultan <tu201906251@code.edu.az>
2024-01-05 22:30:47 +0400

    'feat: maked relation betwen entities and added dtos'

--- EasyPay.DataAccess/Migrations/20240105182603_mig_relation_appUser_customerAccount.Designer.cs
+++ EasyPay.DataAccess/Migrations/20240105182603_mig_relation_appUser_customerAccount.Designer.cs
@@ -4,6 +4,7 @@ using EasyPay.DataAccess.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyPay.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240105182603_mig_relation_appUser_customerAccount")]
+    partial class mig_relation_appUser_customerAccount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EasyPay.EntityLayer/Concrete/CustomerAccount.cs
+++ EasyPay.EntityLayer/Concrete/CustomerAccount.cs
@@ -13,5 +13,8 @@ namespace EasyPay.Entity.Concrete
         public string Currency { get; set; }
         public decimal Balance { get; set; }
         public string BankBranch { get; set; }
+
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
