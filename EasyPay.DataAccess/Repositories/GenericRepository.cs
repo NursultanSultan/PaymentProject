190c34232d4727c973b14e70889bcdf88a2d65ad
NursultanSultan <tu201906251@code.edu.az>
2024-01-06 17:49:07 +0400

    'feat: Established Repository pattern'

---
+++ EasyPay.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using EasyPay.DataAccess.Abstract;
+using EasyPay.DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPay.DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public AppDbContext _context;
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T t)
+        {
+            _context.Set<T>().Remove(t);
+            _context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            _context.Set<T>().Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _context.Set<T>().Update(t);
+            _context.SaveChanges();
+        }
+    }
+}
